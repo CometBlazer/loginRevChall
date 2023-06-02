684a80dadf647eef1f5704d9433a6d366f51169e
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-06-02 11:52:13 -0400

    changed solution

--- login.js
+++ login.js
@@ -10,16 +10,16 @@ window.onload = function() {
             return num / 2 + fee(num);
         }
         function fee(num){
-            return foo(num * 3, square(num));
+            return foo(num * 5, square(num));
         }
         function foo(x, y){
-            return x*x + y*y;
+            return x*x + y*y + 2*x*y;
         }
         function square(num){
             return num * num;
         }
 
-        var key = [47520363.5, 200661429.5, 121649902.5, 108336903, 181185098, 214490710.5];
+        var key = [32421672.5, 160022555, 197009354, 184036413, 165791431.5, 110250050, 203747134.5, 106007665.5, 114618486.5, 1401872, 20702532.5, 1401872, 37896374, 133402552.5, 197009354, 197009354, 148937670, 114618486.5, 1401872, 20702532.5, 160022555, 97891284.5, 184036413, 106007665.5, 128504948, 232440576.5, 4648358, 1401872, 58522542.5, 171714872, 190440057.5, 114618486.5, 197009354, 1401872, 55890618, 128504948, 114618486.5, 1401872, 26071270.5, 190440057.5, 197009354, 97891284.5, 101888885, 148937670, 133402552.5, 190440057.5, 128504948, 114618486.5, 110250050, 1401872, 44036535.5, 184036413, 110250050, 114618486.5, 184036413, 4648358, 1401872, 20702532.5, 160022555, 110250050, 1401872, 26071270.5, 210656255, 114618486.5, 184036413, 232440576.5, 197009354, 128504948, 133402552.5, 160022555, 123743427.5, 1401872, 21958629, 114618486.5, 106007665.5, 165791431.5, 154405530.5, 114618486.5, 190440057.5, 1401872, 23271009.5, 128504948, 97891284.5, 165791431.5, 190440057.5, 1572532.5, 1572532.5];
 
         function validatePassword(password){
             var encryption = password.split('').map(function(char) {
@@ -27,9 +27,6 @@ window.onload = function() {
             });
             var checker = [];
             for (var i = 0; i < encryption.length; i++) {
-                if(!(encryption[i] >= 65 && encryption[i] <= 90) && !(encryption[i] >= 97 && encryption[i] <= 122)){
-                    return false;
-                }
                 var a = encryption[i];
                 var b = fii(a);
                 checker.push(b);
@@ -57,8 +54,6 @@ window.onload = function() {
             var websiteNames = ["Google", "YouTube", "Minecraft", "Discord", "Twitter"];
             var websiteURLs = ["https://www.google.com", "https://www.youtube.com", "https://www.minecraft.net", "https://www.discord.com", "https://www.twitter.com"];
             var randomNum = Math.floor(Math.random() * websiteNames.length);
-
-
             alert("Login successful. Redirecting to " + websiteNames[randomNum] + "...");
             window.location.href = websiteURLs[randomNum];
         } else {
